c1f91efe01eb6b860dc08867e2dc073dbba55658
Tiago Martins <82526424+Tiago-Mart@users.noreply.github.com>
2022-04-02 14:36:59 -0300

    Delete assets directory

--- assets/js/inputs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    const menuBtn = $(".menu-button")
-
-    menuBtn.on("click", function(){
-        $(this).hasClass("clicado")? $(this).removeClass("clicado") : $(this).addClass("clicado");
-    })
-})
