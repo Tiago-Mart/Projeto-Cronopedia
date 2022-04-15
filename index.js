84c5f3b033ea2b19bac3e072aa407df645f0b78e
Vinicin1101 <vserafim_o@outlook.com>
2022-04-15 14:14:40 -0300

    rotas basicas
    + rota 403 e 404
    + rota /search

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const express = require("express");
 const app = express();
 
 // Body parsing
-const body_parser = require("body-parser");
+const bodyParser = require("body-parser");
 
 // Definindo a pasta com os arquivos estaticos do projeto
 const path = require("path");
-var htmlPath = path.join(__dirname + "/Paginas");
+let htmlPath = path.join(__dirname + "/Paginas");
 
 // dotenv
 require("dotenv").config();
@@ -31,7 +31,7 @@ async function main() {
   const collection = db.collection(COLLECTION);
 
   // Formatando o corpo do requerimento com body-parser
-  app.use(body_parser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use(express.static(htmlPath));
 
@@ -39,7 +39,48 @@ async function main() {
   app.get("/", (req, res) => {
     express.static(htmlPath);
   });
+
+  // Servindo a solicitação de pesquisa
+  app.get("/search", (req, res) => {
+    const q = req.query.q;
+
+    if (q) {
+      res.send(`<h1>Pesquisa realizada com sucesso</h1> <p>Termo solicitado: ${q}</p>`);
+    }
+    res.send("<h1>Pesquisa não realizada</h1> <p>Nenhum termo foi solicitado</p>");
+  });
+
+  // Servindo a pagina de teste
+  app.get("/teste", (req, res) => {
+    res.status(200).sendFile(path.join(htmlPath + "/teste.html"));
+  });
+
+  // Servindo a pagina de acesso restrito
+  app.post("/acesso-restrito", (req, res) => {
+    const { nome, email } = req.body;
+
+    if (email == process.env.EMAIL && nome === process.env.NOME) {
+      // se for igual a 1
+      res.send("<h1>Acesso Autorizado</h1>");
+    } else {
+      // se não for igual a 1, retornar a pagina principal
+      res.sendFile(path.join(htmlPath + "/errors/403.html"));
+    }
+  });
+
+  // Servindo a pagina de erro 403 (acesso negado)
+  app.get("/403.html", (req, res) => {
+    res.status(403);
+    res.sendFile(path.join(htmlPath + "/errors/403.html"));
+  });
+
+  // Servindo a pagina de erro 404 (pagina não encontrada)
+  app.get("*", (req, res) => {
+    res.status(404);
+    res.sendFile(path.join(htmlPath + "/errors/404.html"));
+  });
 }
+
 main();
 
 console.log("Listening port: " + port);
